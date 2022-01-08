1f11dff9e304a96ddd6da3083b071ac3d7d9e01d
Ralf Dolsma <ralf.dolsma@gmail.com>
2022-01-08 11:09:44 +0100

    add addition program

---
+++ print_sum_of_2_numbers.py
@@ -0,0 +1,12 @@
+# program adds 2 numbers and prints the sum
+#
+
+# 2 constants
+num1 = 1.5
+num2 = 6.3
+
+# calculate the sum
+sum = num1 + num2
+
+# prints the sum
+print('The sum of num1 and num2 is:', sum )
